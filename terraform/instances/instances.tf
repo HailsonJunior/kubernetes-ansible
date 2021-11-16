6d2a78509920de510cc84c45e1b08735a264d63c
Hailson Ferreira da Silva Junior <hailson.silva@compasso.com.br>
2021-11-16 16:40:40 -0300

    Criando key pair

--- terraform/instances/instances.tf
+++ terraform/instances/instances.tf
@@ -137,3 +137,8 @@ resource "aws_instance" "haproxy" {
         Name = "HAProxy"
     }
 }
+
+resource "aws_key_pair" "my-key" {
+    key_name = "my-key"
+    public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
