51a0972ba37a6cdba039bdd7454a987dcf338375
Hailson Ferreira da Silva Junior <hailson.silva@compasso.com.br>
2021-11-16 16:33:09 -0300

    Ajustes criação das instancias

--- terraform/instances/instances.tf
+++ terraform/instances/instances.tf
@@ -1,30 +1,9 @@
-resource "aws_instance" "K8s" {
-    for_each = {
-        K8s- = {
-            name = "Manager1"
-        }
-        K8s- = {
-            name = "Manager2"
-        }
-        K8s- = {
-            name = "Manager3"
-        }
-        K8s- = {
-            name = "Worker1"
-        }
-        K8s- = {
-            name = "Worker2"
-        }
-        K8s- = {
-            name = "Worker3"
-        }
-    }
-
+resource "aws_instance" "manager1" {
     instance_type = var.k8s_type
     ami = var.ami 
     key_name = "${aws_key_pair.my-key.key_name}"
     network_interface {
-        network_interface_id = 
+        network_interface_id = "${aws_network_interface.network-manager1.id}"
         device_index = 0
     }
 
@@ -35,7 +14,107 @@ resource "aws_instance" "K8s" {
     }
 
     tags = {
-        Name = "${each.key}: ${lookup(each.value, "name", null)}"
+        Name = "K8s - Manager 1"
+    }
+}
+
+resource "aws_instance" "manager2" {
+    instance_type = var.k8s_type
+    ami = var.ami 
+    key_name = "${aws_key_pair.my-key.key_name}"
+    network_interface {
+        network_interface_id = "${aws_network_interface.network-manager2.id}"
+        device_index = 0
+    }
+
+    root_block_device {
+        volume_size = var.block_size_k8s
+        volume_type = var.block_type_k8s
+        delete_on_termination = true
+    }
+
+    tags = {
+        Name = "K8s - Manager 2"
+    }
+}
+
+resource "aws_instance" "manager3" {
+    instance_type = var.k8s_type
+    ami = var.ami 
+    key_name = "${aws_key_pair.my-key.key_name}"
+    network_interface {
+        network_interface_id = "${aws_network_interface.network-manager3.id}"
+        device_index = 0
+    }
+
+    root_block_device {
+        volume_size = var.block_size_k8s
+        volume_type = var.block_type_k8s
+        delete_on_termination = true
+    }
+
+    tags = {
+        Name = "K8s - Manager 3"
+    }
+}
+
+resource "aws_instance" "worker1" {
+    instance_type = var.k8s_type
+    ami = var.ami 
+    key_name = "${aws_key_pair.my-key.key_name}"
+    network_interface {
+        network_interface_id = "${aws_network_interface.network-worker1.id}"
+        device_index = 0
+    }
+
+    root_block_device {
+        volume_size = var.block_size_k8s
+        volume_type = var.block_type_k8s
+        delete_on_termination = true
+    }
+
+    tags = {
+        Name = "K8s - Worker 1"
+    }
+}
+
+resource "aws_instance" "worker2" {
+    instance_type = var.k8s_type
+    ami = var.ami 
+    key_name = "${aws_key_pair.my-key.key_name}"
+    network_interface {
+        network_interface_id = "${aws_network_interface.network-worker2.id}"
+        device_index = 0
+    }
+
+    root_block_device {
+        volume_size = var.block_size_k8s
+        volume_type = var.block_type_k8s
+        delete_on_termination = true
+    }
+
+    tags = {
+        Name = "K8s - Worker 2"
+    }
+}
+
+resource "aws_instance" "worker3" {
+    instance_type = var.k8s_type
+    ami = var.ami 
+    key_name = "${aws_key_pair.my-key.key_name}"
+    network_interface {
+        network_interface_id = "${aws_network_interface.network-worker3.id}"
+        device_index = 0
+    }
+
+    root_block_device {
+        volume_size = var.block_size_k8s
+        volume_type = var.block_type_k8s
+        delete_on_termination = true
+    }
+
+    tags = {
+        Name = "K8s - Worker 3"
     }
 }
 
@@ -44,7 +123,7 @@ resource "aws_instance" "haproxy" {
     ami = var.ami
     key_name = "${aws_key_pair.my-key.key_name}"
     network_interface {
-        network_interface_id =
+        network_interface_id = "${aws_network_interface.network-haproxy.id}"
         device_index = 0
     }
 
