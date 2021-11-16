6b95376b2bbef7d7803dbb6cf601fe662985ab20
Hailson Ferreira da Silva Junior <hailson.silva@compasso.com.br>
2021-11-16 16:34:19 -0300

    Correção sintaxe

--- terraform/instances/network.tf
+++ terraform/instances/network.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "k8s-sg" {
         from_port = 0
         to_port = 0
         protocol = -1
-        cidr_blocks ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
