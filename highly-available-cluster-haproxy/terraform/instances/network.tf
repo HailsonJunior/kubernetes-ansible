cdbba37cbf5bd378327e0a6a37e445834fe9a0a4
Hailson Junior <79525895+HailsonJunior@users.noreply.github.com>
2022-01-28 17:25:29 -0300

    Update network.tf

--- highly-available-cluster-haproxy/terraform/instances/network.tf
+++ highly-available-cluster-haproxy/terraform/instances/network.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "k8s-sg" {
         from_port = 0
         to_port =0
         protocol = -1
-        cidr_blocks = ["172.31.0.0/16"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
         from_port = 0
